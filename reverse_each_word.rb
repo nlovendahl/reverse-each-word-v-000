790d1952440a5aa1a54572d95e2b8c43f1b0710c
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-20 04:01:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,6 @@ def reverse_each_word(string)
   reverse_strings = []
   string.each do |element|
     reverse_strings << element.reverse!
-    reverse_strings
   end
   reverse_strings.join(" ")
   reverse_strings
